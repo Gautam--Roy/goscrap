23921cbe05da5c62c225093fb0b26e261e17f9e0
Gautam Roy <zademathews@gmail.com>
2024-06-18 00:16:24 +0000

    Added sitemap check

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/xml"
 	_fl "flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -31,44 +33,96 @@ func main() {
 	}
 
 	if flag.Url != "" {
-		header := ""
-		url := flag.Url
-		switch flag.ReturnType {
-		case "mdx":
-			header = "markdown"
-			url = "https://r.jina.ai/" + flag.Url
-		case "text":
-			header = "text"
-			url = "https://r.jina.ai/" + flag.Url
-		default:
-			header = "html"
-		}
-
-		req, err := http.NewRequest("GET", url, nil)
-		req.Header.Set("X-Return-Format", header)
-		if err != nil {
-			log.Fatal("Invalid url")
-		}
-
-		client := http.Client{}
-		res, err := client.Do(req)
-		if err != nil {
-			log.Fatal("Error while processing request:", err.Error())
-		}
-
-		if res.StatusCode != 200 {
-			log.Fatal("Non-success status code received: ", res.StatusCode)
-		}
-
-		bytes, err := io.ReadAll(res.Body)
-		if err != nil {
-			log.Fatal("Error reading response body: ", err.Error())
-		}
-
-		log.Print(string(bytes))
-
-		res.Body.Close()
-		os.Exit(0)
+		scrapUrl(flag.Url, flag.ApiKey, flag.ReturnType)
 	}
 
+	if flag.Sitemap != "" {
+		siteMapUrl := flag.Sitemap
+
+		if isSiteMap(siteMapUrl) {
+			fmt.Println("Sitemap confirmed")
+		} else {
+			log.Fatal("Invalid sitemap")
+			os.Exit(0)
+		}
+
+		res, err := http.Get(siteMapUrl)
+		if err != nil {
+			panic(err)
+		}
+
+		var urlSet struct {
+			URLs []struct {
+				Loc string `xml:"loc"`
+			} `xml:"url"`
+		}
+
+		if err = xml.NewDecoder(res.Body).Decode(&urlSet); err != nil {
+			log.Fatal("There was an error while Decoding xml file at ScrapToFile: ", err.Error())
+		}
+
+		fmt.Println(urlSet.URLs[0])
+		os.Exit(0)
+	}
+}
+
+func isSiteMap(url string) bool {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	return res.Header.Get("Content-Type") == "application/xml"
+}
+
+func scrapUrl(url string, api string, returnType string) {
+
+	var header string
+	var _url string
+
+	switch returnType {
+	case "mdx":
+		header = "markdown"
+		_url = "https://r.jina.ai/" + url
+	case "text":
+		header = "text"
+		_url = "https://r.jina.ai/" + url
+	default:
+		header = "html"
+		api = ""
+		_url = url
+	}
+
+	req, err := http.NewRequest("GET", _url, nil)
+	req.Header.Set("X-Return-Format", header)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", api))
+	if err != nil {
+		log.Fatal("Invalid url")
+	}
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error while processing request:", err.Error())
+	}
+
+	if res.StatusCode != 200 {
+		log.Fatal("Non-success status code received: ", res.StatusCode)
+	}
+
+	bytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Error reading response body: ", err.Error())
+	}
+
+	log.Print(string(bytes))
+
+	res.Body.Close()
+	os.Exit(0)
 }
